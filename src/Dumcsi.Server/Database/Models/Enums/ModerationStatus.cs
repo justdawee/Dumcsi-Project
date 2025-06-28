b15f3e9ce8be9dbc66bb0e41a0fd6d63da0eee15
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2025-06-28 14:10:38 +0200

    Delete src/Dumcsi.Server directory

--- src/Dumcsi.Server/Database/Models/Enums/ModerationStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dumcsi.Server.Database.Models.Enums;
-
-public enum ModerationStatus
-{
-    Visible,          // Látható
-    UserDeleted,      // Felhasználó által törölt
-    ModeratedRemoved  // Moderátor által elrejtett
-}
--- src/Dumcsi.Server/Services/JWTFactory/JWTFactoryOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dumcsi.Server.Services.JWTFactory;
-
-public sealed class JWTFactoryOptions
-{
-    public string Secret { get; set; } = null!;
-    public string Issuer { get; set; } = null!;
-    public string Audience { get; set; } = null!;
-}
