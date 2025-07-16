3feb14e53b66390893d744243b019bdfefae54e8
Lukács Dávid <17357366+justdawee@users.noreply.github.com>
2025-07-17 00:24:25 +0200

    refactor: Update project files to target .NET 9.0; adjust package references and add Dockerfile

--- src/Dumcsi.Tests/Integration/UnitTest1.cs
+++ src/Dumcsi.Tests/Integration/UnitTest1.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Xunit.Abstractions;
 
 namespace Dumcsi.Tests.Integration;
